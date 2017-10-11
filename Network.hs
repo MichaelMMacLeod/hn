d04751f08e684944077ae3c8ce0e6740310cb222
Michael MacLeod <michaelmmacleod@gmail.com>
2017-10-10 21:58:01 -0700

    Rename inputs to activations

--- Network.hs
+++ Network.hs
@@ -7,7 +7,7 @@ module Network
 import Matrix
 
 data Net = Net 
-    { inputs :: Matrix Double
+    { activations :: Matrix Double
     , weights :: [Matrix Double]
     , biases :: [Matrix Double]
     } 
@@ -17,21 +17,21 @@ sigmoid :: Floating a => a -> a
 sigmoid x = 1 / (1 + exp (-x))
 
 output :: Floating a => Matrix a -> Matrix a -> Matrix a -> Matrix a
-output i w b = w * i + b
+output a w b = w * a + b
 
 activate :: Floating a => Matrix a -> Matrix a
 activate = fmap sigmoid
 
 propStep :: Floating a => Matrix a -> Matrix a -> Matrix a -> Matrix a
-propStep i w b = activate (output i w b)
+propStep a w b = activate (output a w b)
 
 propagate :: Net -> [Matrix Double]
 propagate (Net _ [] _) = []
 propagate (Net _ _ []) = []
-propagate (Net i (w : ws) (b : bs)) =
+propagate (Net a (w : ws) (b : bs)) =
     layer : propagate (Net layer ws bs)
     where
-        layer = propStep i w b
+        layer = propStep a w b
 
 readNet :: FilePath -> IO Net
 readNet f = fmap read (readFile f)
