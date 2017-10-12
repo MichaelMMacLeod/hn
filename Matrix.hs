cee5a6fe35af79e6133e3a21442ac8ed5cb16b5b
Michael MacLeod <michaelmmacleod@gmail.com>
2017-10-11 20:26:16 -0700

    Add several matrix operations

--- Matrix.hs
+++ Matrix.hs
@@ -1,21 +1,44 @@
-module Matrix 
+module Matrix
     (   Matrix (..)
+    ,   (|*|)
+    ,   (*|)
+    ,   (|^)
+    ,   (|^^)
+    ,   (|**)
     )   where
 
-import Data.List (transpose)
+    import Data.List (transpose)
 
-newtype Matrix a = Matrix [[a]] deriving (Read, Show, Eq)
+    newtype Matrix a = Matrix [[a]] deriving (Read, Eq, Show)
 
-instance Num a => Num (Matrix a) where
-    Matrix a + Matrix b = Matrix (zipWith (zipWith (+)) a b)
-    Matrix a - Matrix b = Matrix (zipWith (zipWith (-)) a b)
-    Matrix a * Matrix b = 
-        Matrix [[sum $ zipWith (*) a' b' | b' <- transpose b] 
-                                         | a' <- a ]
-    negate (Matrix a) = Matrix (map (map negate) a)
-    fromInteger x = Matrix (iterate (0:) (fromInteger x : repeat 0))
-    abs x = x
-    signum _ = 1
+    instance Functor Matrix where
+        fmap f (Matrix a) = Matrix (map (map f) a)
 
-instance Functor Matrix where
-    fmap f (Matrix a) = Matrix (map (map f) a)
+    instance (Num a) => Num (Matrix a) where
+        Matrix a + Matrix b = Matrix (zipWith (zipWith (+)) a b)
+        Matrix a - Matrix b = Matrix (zipWith (zipWith (-)) a b)
+        Matrix a * Matrix b = 
+            Matrix [[sum $ zipWith (*) a' b' | b' <- transpose b] 
+                                             | a' <- a ]
+        negate (Matrix a) = Matrix (map (map negate) a)
+        fromInteger x = Matrix (iterate (0:) (fromInteger x : repeat 0))
+        abs x = x
+        signum _ = 1
+
+    -- Hadamard product
+    (|*|) :: (Num a) => Matrix a -> Matrix a -> Matrix a
+    Matrix a |*| Matrix b = Matrix (zipWith (zipWith (*)) a b)
+
+    -- Multiply by a scalar value
+    (*|) :: (Num a) => a -> Matrix a -> Matrix a
+    a *| m = fmap (*a) m
+
+    -- Power functions
+    (|^) :: (Num a, Integral b) => Matrix a -> b -> Matrix a
+    m |^ a = fmap (^a) m
+
+    (|^^) :: (Fractional a, Integral b) => Matrix a -> b -> Matrix a
+    m |^^ a = fmap (^^a) m
+
+    (|**) :: (Floating a) => Matrix a -> a -> Matrix a
+    m |** a = fmap (**a) m
