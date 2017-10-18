8e14a87d33a5d13c5c8f79b661d00fcf25df7c5c
Michael MacLeod <michaelmmacleod@gmail.com>
2017-10-18 15:07:52 -0700

    Generate randomized networks in Main

--- Main.hs
+++ Main.hs
@@ -1,23 +1,13 @@
-module Main where
+module Main (main) where
 
-import Network
-import Matrix
+    import Network
+    import System.Environment (getArgs)
 
-main :: IO ()
-main = do
-    net <- readNet "net.txt" :: IO (Net Double)
-    tdata <- readTrainingData "training.txt" :: IO (Training Double)
-    let trained = last $ take 1000 (train net tdata)
+    generate :: FilePath -> [Int] -> IO ()
+    generate file layers = do
+        net <- randNet layers :: IO (Net Double)
+        writeNet file net
 
-    putStr "Target values: "
-    print $ targets tdata
-
-    putStr "Old activation: "
-    print $ last (activations net (inputs tdata))
-
-    putStr "Trained activations: "
-    print $ last (activations trained (inputs tdata))
-
-    putStr "Writing new network to net.txt..."
-    writeNet "net.txt" trained
-    putStrLn "Done."
+    main = do
+        args <- getArgs
+        generate (head args) (read (last args)) 
